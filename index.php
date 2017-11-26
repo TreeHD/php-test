22d6c49cf5589801b85eceb1173ac22c9aa52366
unknown <mini87479@gmail.com>
2017-11-26 17:00:02 +0800

    3st

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
-"dingerkaigan"
-?>
+session_start();
+include('sql.inc.php');
+@$no = $_GET['no'];
+if ($_SESSION['admin'] != '1' OR $_SESSION['admin'] == null){
+$_SESSION['msg'] == 'nopex';
+header("Location: ../dashboard"); 
+exit;
+}else{
+$sql = "DELETE FROM `svList_op` WHERE No = $no";
+if(mysql_query($sql)){
+$_SESSION['msg'] = "delopok";
+}else{
+$_SESSION['msg'] = "delopno";
+}
+header("Location: ../reportlist"); 
+exit;
+}
